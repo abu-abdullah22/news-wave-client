67494150db79952c9eab64f75842286ec7f28b19
abu-abdullah22 <rorschachmayhem@gmail.com>
2024-06-03 22:04:57 +0600

    normal routes also added in dashboard

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -13,15 +13,23 @@ const Dashboard = () => {
             <div className="drawer-side">
                 <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
                 <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-                    {/* Sidebar content here */}
+                    {/* dashboard special routes here */}
                     <li><NavLink to={'/dashboard/allUsers'}>All Users</NavLink></li>
-                    <li><NavLink to={'/dashboard/allArticlesApproval'}>All Articles</NavLink></li>
+                    <li><NavLink to={'/dashboard/allArticlesApproval'}>All Articles (Approval)</NavLink></li>
                     <li><NavLink to={'/dashboard/addPublishers'}>Add Publishers</NavLink></li>
+                    <div className="divider"></div>
+                    {/* normal routes here */}
+                    <li><NavLink to={'/'}>Home</NavLink></li>
+                    <li><NavLink to={'/addArticles'}>Add Articles</NavLink></li>
+                    <li><NavLink to={'/allArticles'}>All Articles</NavLink></li>
+                    <li><NavLink to={'/subscription'}>Subscription</NavLink></li>
+                    <li> <NavLink to={'/myArticles'}>My Articles</NavLink></li>
+                    <li><NavLink to={'/premiumArticles'}>Premium Articles</NavLink></li>
                 </ul>
 
             </div>
 
-           
+
         </div>
     );
 };
