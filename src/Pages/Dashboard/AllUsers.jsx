433f308a792f9cec0894baa57c9c119876840ab9
abu-abdullah22 <rorschachmayhem@gmail.com>
2024-06-03 22:46:50 +0600

    loaded user data

--- src/Pages/Dashboard/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers.jsx
@@ -1,7 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+
 const AllUsers = () => {
+    const axiosSecure = useAxiosSecure() ;
+    const {data: users = []} = useQuery({
+        queryKey: ['allUsers'],
+        queryFn: async()=> {
+            const res = await axiosSecure.get('/users') ;
+            return res.data ;
+        }
+    }) 
     return (
         <div>
-            <h2>All users here</h2>
+            <h2>All users here ; {users.length}</h2>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,16 @@ import { RouterProvider } from 'react-router-dom'
 import router from './Routes/Routes.jsx'
 import AuthProvider from './Provider/AuthProvider.jsx'
 import { Toaster } from 'react-hot-toast'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <AuthProvider>
       <RouterProvider router={router}></RouterProvider>
     </AuthProvider>
+    </QueryClientProvider>
     <Toaster />
   </React.StrictMode>,
 )
