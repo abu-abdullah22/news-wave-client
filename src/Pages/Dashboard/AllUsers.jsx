4b934c4dbc6a799954985ba089b1b63fc4f51001
abu-abdullah22 <rorschachmayhem@gmail.com>
2024-06-04 00:30:14 +0600

    added admin

--- src/Pages/Dashboard/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers.jsx
@@ -1,18 +1,65 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import toast from "react-hot-toast";
+import { FaUsers } from "react-icons/fa";
 
 const AllUsers = () => {
-    const axiosSecure = useAxiosSecure() ;
-    const {data: users = []} = useQuery({
+    const axiosSecure = useAxiosSecure();
+    const { data: users = [], refetch } = useQuery({
         queryKey: ['allUsers'],
-        queryFn: async()=> {
-            const res = await axiosSecure.get('/users') ;
-            return res.data ;
+        queryFn: async () => {
+            const res = await axiosSecure.get('/users');
+            return res.data;
         }
-    }) 
+    })
+
+    const hanldeMakeAdmin = user => {
+        axiosSecure.patch(`/users/admin/${user._id}`)
+            .then(res => {
+                console.log(res.data);
+                if (res.data.modifiedCount > 0) {
+                    refetch();
+                    toast.success(`Admin added`)
+                }
+            });
+    }
     return (
         <div>
             <h2>All users here ; {users.length}</h2>
+            <div className="flex justify-evenly my-4">
+                <h2 className="text-3xl">All Users </h2>
+                <h2 className="text-3xl">Total Users : {users.length} </h2>
+            </div>
+            <div className="overflow-x-auto">
+                <table className="table table-zebra ">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Photo</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, index) => <tr key={user._id}>
+                                <th>{index + 1}</th>
+                                <td>{user.name}</td>
+                                <td><img src={user?.photo} referrerPolicy="no-referrer" className="w-[60px]" alt="" /></td>
+                                <td>{user.email}</td>
+                                <td>
+                                    <td>
+                                        {user.role === 'admin' ? 'Admin' : <button onClick={() => hanldeMakeAdmin(user)} className="btn btn-lg bg-[#D1A054]"><FaUsers className="text-white text-xl"></FaUsers></button>}
+                                    </td>
+                                </td>
+
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -4,7 +4,7 @@ const Dashboard = () => {
     return (
         <div className="drawer lg:drawer-open">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content flex flex-col items-center justify-center">
+            <div className="drawer-content flex flex-col m-4">
                 {/* Page content here */}
                 <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open dashboard</label>
                 <Outlet></Outlet>
@@ -12,7 +12,7 @@ const Dashboard = () => {
             </div>
             <div className="drawer-side">
                 <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+                <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content ">
                     {/* dashboard special routes here */}
                     <li><NavLink to={'/dashboard/allUsers'}>All Users</NavLink></li>
                     <li><NavLink to={'/dashboard/allArticlesApproval'}>All Articles (Approval)</NavLink></li>
